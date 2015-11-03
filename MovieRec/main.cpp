5856430afed2593732030f3b412c9f225330abb1
Anton Krasnokutskiy <krasnokutskiy.anton@gmail.com>
2015-11-03 12:14:35 +0200

    Initial Commit

---
+++ MovieRec/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MovieRec
+//
+//  Created by Anton Krasnokutskiy on 03.11.15.
+//  Copyright (c) 2015 Anton Krasnokutskiy. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
